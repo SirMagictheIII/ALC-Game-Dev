f5f462e95443cd20757a64ef2e4e19a9223bd0e7
Cole Carson <sircarson4@gmail.com>
2020-09-25 12:27:41 -0600

    make

---
+++ PROJECT/Assets/scripts/playercontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    //access modifier type name operator value
+    private int speed = 20;
+    private float turnSpeed = 25;
+    
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+    horizontalInput = Input.GetAxis("Horizontal");
+    forwardInput = Input.GetAxis("Vertical");
+    //moves teh vehicle forwards based on forwardInput
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    //Rotates the vehicle left and right based on horizontal input
+    transform.Rotate(Vector3.up, turnSpeed * horitzontalInput * Time.deltaTime);
+        
+    }
+}
