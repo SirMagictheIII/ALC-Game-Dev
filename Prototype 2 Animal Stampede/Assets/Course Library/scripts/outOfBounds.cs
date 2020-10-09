fce9e09976ad25a1a375b8aad4c8375d6fcb8537
Cole Carson <sircarson4@gmail.com>
2020-10-09 12:37:08 -0600

    updated protoype

---
+++ Prototype 2 Animal Stampede/Assets/Course Library/scripts/outOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outOfBounds : MonoBehaviour
+{
+    public float topBounds = 35f;
+    public float bottomBounds = -15f;
+    ///working on a solution for error 234
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+        if(transform.position.z < bottomBounds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2 Animal Stampede/Assets/Course Library/scripts/playerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 20f;
+    //allows for the change of range
+    public GameObject foodMissile;
+    //allows for thorwing food
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        
+        //if -xRange with xRange are on this line, it loops like Pacman
+        if(transform.position.x < -xRange)
+        { //constrains leftward movement x @ -10
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        
+        }
+        if(transform.position.x > xRange)
+        { //constrains leftward movement x @ -10
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodMissile, transform.position, foodMissile.transform.rotation);
+        }
+    }
+}
