389f1b4f1e999500ba986c9faa52214a68f6ed33
Cole Carson <sircarson4@gmail.com>
2020-11-05 12:44:30 -0700

    prototype 3

---
+++ Project #3/Assets/scripts/spawning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawning : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(30, 0 ,-2);
+    private float startDelay = 2.0f;
+    private float repeatDelay = 10.0f;
+    
+    // (x, y, z)
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+    }
+    void SpawnObstacle()
+    {
+    Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
