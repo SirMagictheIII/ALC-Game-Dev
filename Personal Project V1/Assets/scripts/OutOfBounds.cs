f7f224d5d963c95b06c7ae05fb81a80483ee910d
Carson <sircarson>
2020-12-23 18:51:23 -0700

    Personal P. Code

---
+++ Personal Project V1/Assets/scripts/OutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public float sideBounds = 8f;
+    public float TopBounds = 9f;
+    //creates boundaries so the player cannot escape the map
+
+
+    void Update()
+    {
+        if(transform.position.x < -sideBounds)
+           transform.position = new Vector3(-sideBounds, transform.position.y, transform.position.z);
+
+        if(transform.position.x > sideBounds)
+            transform.position = new Vector3(sideBounds, transform.position.y, transform.position.z);
+
+        if (transform.position.z > TopBounds)
+            transform.position = new Vector3(transform.position.x, transform.position.y, TopBounds);
+
+        if (transform.position.z < -TopBounds)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -TopBounds);
+    }
+}
---
+++ Personal Project V1/Assets/scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 2;
+
+    public float horizontalInput;
+    public float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //allows player to move forwards, or side to side based on the keypad.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
